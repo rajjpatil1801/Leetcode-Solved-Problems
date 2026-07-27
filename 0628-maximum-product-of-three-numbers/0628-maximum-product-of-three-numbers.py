442989bfa752210f1688767e1878c6a4357518ee
Raj Patil <rajjpatil1801@gmail.com>
2026-07-27 03:31:42 -0700

    Time: 23 ms (40.38%), Space: 20.4 MB (42.3%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
@@ -4,6 +4,4 @@ class Solution:
         l = len(nums)
         p1 = nums[0]*nums[1]*nums[l-1]
         p2 = nums[l-1]*nums[l-2]*nums[l-3]
-        if(p1>p2):
-            return p1
-        return p2
+        return max(p1,p2)
